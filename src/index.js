8c0b837b1c3042c05bbe40ebacffe84f82850c68
Rafal Wilinski <raf.wilinski@gmail.com>
2018-06-20 07:42:33 +0200

    Add react-ga

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+ReactGA.initialize(process.env.REACT_APP_GA_KEY);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
