4d4cb037a46a00265058781a3f184823201b357d
Rafal Wilinski <raf.wilinski@gmail.com>
2018-07-03 21:54:36 +0200

    Add basic /about and verification

--- src/About.js
+++ src/About.js
@@ -1,3 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
-export default () => <div>About</div>;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 20px;
+`;
+
+export default () => (
+  <Container>
+    <h1>⛺️ Wildspot</h1>
+    <h4>Find your next sweet spot for setting up a tent</h4>
+    <p>
+      This project is brought to you by{" "}
+      <a href="https://rwilinski.me">Rafal Wilinski</a>. Wildspot is 100% open
+      source, you can check it's code
+      <a href="https://github.com/RafalWilinski/wildspot"> here.</a>
+    </p>
+  </Container>
+);
